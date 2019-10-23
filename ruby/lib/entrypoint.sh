a1a659b5c5e4a966779b6f7ca1dd91780810b1db
Lukas Gentele <lg@devspace.cloud>
2019-10-23 09:04:54 -0700

    improve rails template

---
+++ ruby/lib/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+if [ -f /app/tmp/pids/server.pid ]; then
+  rm /app/tmp/pids/server.pid
+fi
+
+bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
+
+exec bundle exec "$@"
