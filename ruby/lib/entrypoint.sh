3625f42e7edf9e3878df9e10bddc99a87f5cb1c3
Lukas Gentele <lg@devspace.cloud>
2019-10-23 09:54:50 -0700

    improve rails entrypoint

--- ruby/lib/entrypoint.sh
+++ ruby/lib/entrypoint.sh
@@ -5,6 +5,6 @@ if [ -f /app/tmp/pids/server.pid ]; then
   rm /app/tmp/pids/server.pid
 fi
 
-bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup || echo "Unable to setup database"
+bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
 
 exec bundle exec "$@"
